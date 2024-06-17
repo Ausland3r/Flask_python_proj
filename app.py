712d376258bd9d1c29c248b95ff5b2a403f14f98
Ausland3r <Ausland3r@yandex.ru>
2024-06-18 00:21:26 +0300

    prj v.1 recruto

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_recruto():
+    name = request.args.get('name')
+    message = request.args.get('message')
+    if not name or not message:
+        return "<html><body><h1>Пожалуйста, укажите параметры 'name' и 'message' в URL.</h1></body></html>"
+    return render_template('hello.html', name=name, message=message)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
