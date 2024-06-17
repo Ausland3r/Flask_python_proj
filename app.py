6bfb3efb666f5d25f99e7f9eddbb2299a992f20a
Ausland3r <Ausland3r@yandex.ru>
2024-06-18 01:45:51 +0300

    prj v.1 recruto

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
@@ -10,4 +12,5 @@ def hello_recruto():
     return render_template('hello.html', name=name, message=message)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
